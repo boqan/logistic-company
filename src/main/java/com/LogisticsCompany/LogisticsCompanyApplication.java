6a002c96cc2278d21699db13430b52946c18675d
Yasna Mineva <yasna.mineva@sap.com>
2023-11-25 10:50:58 +0200

    initial commit

---
+++ src/main/java/com/LogisticsCompany/LogisticsCompanyApplication.java
@@ -0,0 +1,13 @@
+package com.LogisticsCompany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsCompanyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsCompanyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LogisticsCompany/LogisticsCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LogisticsCompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
