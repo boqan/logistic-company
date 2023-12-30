abc89e0994ace9b2e659c39e56950cf925a65fb6
Teodora Savcheva <teddyxsavcheva02@gmail.com>
2023-12-30 15:30:18 +0200

    Interface EmployeeService and EmployeeRepository

--- src/main/java/com/LogisticsCompany/dto/EmployeeDTOnoOffice.java
+++ src/main/java/com/LogisticsCompany/dto/EmployeeDTOnoOffice.java
@@ -1,13 +1,11 @@
 package com.LogisticsCompany.dto;
 
-
 import com.LogisticsCompany.model.IdGenerator;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
-
 
 @Data
 @AllArgsConstructor
--- src/main/java/com/LogisticsCompany/service/implementation/EmployeeServiceImpl.java
+++ src/main/java/com/LogisticsCompany/service/implementation/EmployeeServiceImpl.java
@@ -2,15 +2,18 @@ package com.LogisticsCompany.service.implementation;
 
 import com.LogisticsCompany.mapper.EntityMapper;
 import com.LogisticsCompany.repository.EmployeeRepository;
+import com.LogisticsCompany.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EmployeeServiceImpl{
+public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
     private EmployeeRepository employeeRepository;
     @Autowired
-    private EntityMapper entityMapper;
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 
 
 
