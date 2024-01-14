652beb63b784c9cae2bf4e639abc3f132f7407cc
boqan <bojan.atanasov2002@gmail.com>
2024-01-14 13:05:11 +0200

    Created order controller, added several DTOs, connected order create with the office entity, so the office's order list also gets updated. Created order service and order repository.
    Other people's code that i touched:
    Added entity mapper methods, will also add exception handler methods for order controller.
    
    Added default office fetching method and office orders updating, so that the orders list in the office stays up to date with the orders table.

--- src/main/java/com/LogisticsCompany/repository/OfficeRepository.java
+++ src/main/java/com/LogisticsCompany/repository/OfficeRepository.java
@@ -1,12 +1,10 @@
 package com.LogisticsCompany.repository;
 
 
-import com.LogisticsCompany.model.Employee;
 import com.LogisticsCompany.model.Office;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
