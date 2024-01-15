02ce2b29c9c91e766d5258d775575a4d68d2169f
Yasna Mineva <yasna.mineva@sap.com>
2024-01-15 17:56:25 +0200

    checkpoint

--- src/main/java/com/LogisticsCompany/controller/ClientController.java
+++ src/main/java/com/LogisticsCompany/controller/ClientController.java
@@ -1,13 +1,12 @@
-package com.LogisticsCompany.controller;
-
-import com.LogisticsCompany.service.ClientService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-
-public class ClientController {
-    @Autowired
-    ClientService clientService;
-
-}
+//package com.LogisticsCompany.controller;
+//
+//import com.LogisticsCompany.service.ClientService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//public class ClientController {
+//    @Autowired
+//    ClientService clientService;
+//
+//}
