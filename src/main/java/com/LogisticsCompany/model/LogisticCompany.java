469d04badc937b8fcd240fdcf4a619277f65c02a
Penchev <36088861+Lordlory95@users.noreply.github.com>
2024-02-01 21:01:26 +0200

    Small fix

--- src/main/java/com/LogisticsCompany/model/LogisticCompany.java
+++ src/main/java/com/LogisticsCompany/model/LogisticCompany.java
@@ -7,6 +7,7 @@ import lombok.*;
 import org.hibernate.query.Order;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -32,7 +33,8 @@ public class LogisticCompany extends IdGenerator {
 
     @Column(name = "offices")
     @OneToMany(mappedBy = "logisticCompany")
-    private List<Office> offices;
+    @Builder.Default
+    private List<Office> offices = new ArrayList<>();
 
 
 
